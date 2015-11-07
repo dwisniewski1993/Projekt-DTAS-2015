f05464c95682b652226c03ab062f086f00b4d04c
infensus <jaaneq@gmail.com>
2015-11-07 20:25:35 +0100

    Dodanie do bazy procedury pobierającej kategorie

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS product_attributes;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS categories_attributes;
+
 CREATE TABLE products (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
@@ -21,3 +26,35 @@ CREATE TABLE categories_attributes (
   `name` VARCHAR(200) NOT NULL,
   `value_type` ENUM('numeric', 'boolean', 'string')
 );
+
+-------
+
+DROP PROCEDURE IF EXISTS category;
+
+DELIMITER //
+CREATE PROCEDURE category(id INT, filter TEXT)
+BEGIN
+  SET @category_id = id;
+  SET @sql = NULL;
+
+  SELECT GROUP_CONCAT(CONCAT(
+      'MAX(IF(ca.name = ''', name, ''', a.value, NULL)) AS ''', name, ''''
+    )) into @sql FROM categories_attributes WHERE category_id = @category_id;
+    
+
+  SET @sql = CONCAT(
+    'SELECT * FROM (
+      SELECT p.id, p.name, ', @sql, ' 
+      FROM products p
+      LEFT JOIN product_attributes AS a ON a.product_id = p.id
+      LEFT JOIN categories_attributes AS ca ON ca.id = a.attribute_id
+      WHERE p.category_id = ', @category_id,
+      ' GROUP BY p.id',
+    ') category ', filter
+  );
+
+  PREPARE stmt FROM @sql;
+  EXECUTE stmt;
+  DEALLOCATE PREPARE stmt;
+END//
+DELIMITER ;
