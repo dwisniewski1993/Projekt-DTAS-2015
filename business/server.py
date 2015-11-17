5e24c67cd0cffef2a0c593b6f32a9ca10d26637c
infensus <jaaneq@gmail.com>
2015-11-17 20:05:05 +0100

    Dodanie funkcjonalności
    Dodawanie kategorii i produktów, lista

--- business/server.py
+++ business/server.py
@@ -1,33 +1,71 @@
 from flask import Flask, request, jsonify, send_from_directory
 from model import *
 from services import Repository
+import pymysql
 
-app = Flask(__name__)
-repo = Repository()
-
-@app.route('/app/<path:path>')
-def index(path):
-    return send_from_directory('../web', path)
+connection = pymysql.connect(host='46.101.220.96', user='root', password='projekt tas', db='tas', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+app = Flask(__name__, static_url_path='/static', static_folder='../web/')
 
 
-@app.route('/products/', methods=['GET'])
-def products():
-    return jsonify({'products': repo.find_products()})
-
-@app.route('/categories/<int:id>/', methods=['GET'])
-def category(id):
-    return jsonify(repo.find_category(id))
-
-@app.route('/products/<int:id>/', methods=['GET'])
-def product(id):
-    return jsonify(repo.find_product(id))
-
-@app.route('/products/<int:id>/reviews', methods=['GET', 'POST'])
-def product_reviews(id):
+@app.route('/api/categories/', methods=['GET', 'POST'])
+def get_categories():
     if request.method == 'GET':
-        return jsonify({'reviews': repo.find_reviews(id)})
-    else:
-        return jsonify(request.json)
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM categories"
+            cursor.execute(sql)
 
+            return jsonify({'categories': cursor.fetchall()})
+    else:
+        new_category = request.json
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO categories(name) VALUES (%s)"
+            cursor.execute(sql, (new_category['name'],))
+            sql = "INSERT INTO categories_attributes(category_id, name, value_type) VALUES (%s,%s,'string')"
+            cursor.executemany(sql, [(cursor.lastrowid,a) for a in new_category['attributes']])
+        connection.commit()        
+
+        return jsonify({'status':'ok'})
+
+@app.route('/api/categories/<int:category_id>/')
+def get_category(category_id):
+    with connection.cursor() as cursor:
+        sql = "SELECT name FROM categories WHERE id = %s"
+        cursor.execute(sql, (category_id,))
+
+        return jsonify(cursor.fetchone())
+
+
+@app.route('/api/categories/<int:category_id>/attributes')
+def categories_attributes(category_id):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM categories_attributes WHERE category_id = %s"
+        cursor.execute(sql, (category_id,))
+
+        return jsonify({'attributes': cursor.fetchall()})
+
+
+@app.route('/api/products/', methods=['POST'])
+def add_product():
+    new = request.json
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO products(name, category_id) VALUES (%s,%s)"
+        cursor.execute(sql, (new['name'],new['category_id']))
+        sql = "INSERT INTO product_attributes VALUES (%s,%s,%s)"
+        cursor.executemany(sql, [(cursor.lastrowid,k,v) for k,v in new['attributes'].items()])
+    connection.commit()        
+
+    return jsonify({'status':'ok'})
+
+@app.route('/api/categories/<int:category_id>/products')
+def get_category_products(category_id):
+    with connection.cursor() as cursor:
+        cursor.callproc('category', args=(category_id, ''))
+        return jsonify({'products': cursor.fetchall()})
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return app.send_static_file('index.html')
 
 app.run(debug=True)
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,47 +1,204 @@
-var App = React.createClass({
+var {Router, Route, IndexRoute, Link} = ReactRouter;
+var {createHistory} = window.History;
+
+var TopBar = (props) =>
+    <div id="top-bar-wrap">
+        <header id="top-bar">
+            <span id="logo">Serwis do oceniania</span>
+            <div id="user">
+                <span>{props.username}</span>
+                <div id="avatar">xD</div>
+            </div>
+        </header>
+    </div>;
+
+var MainMenu = () =>
+    <div id="main-nav-wrap">
+        <nav id="main-nav">
+            <Link activeClassName="active" to="/produkty">Lista produktów</Link>
+            <Link activeClassName="active" to="/produkty/dodaj">Dodaj produkt</Link>
+            <Link activeClassName="active" to="/kategorie/dodaj">Dodaj kategorię</Link>
+        </nav>
+    </div>
+
+// -----------------------------------------------------------------
+
+var CategoryForm = React.createClass({
+    getInitialState: function() {
+        return {attributes: [], name: ''};
+    },
+    updateTitle: function() {
+        this.setState({name: this.refs.nazwa.value});
+    },
+    addAttribute: function(event) {
+        if (event.keyCode != 13) return;
+
+        var newAttrs = this.state.attributes.concat([this.refs.atrybut.value]);
+        this.setState({attributes: newAttrs});
+        this.refs.atrybut.value = '';
+    },
+    addCategory: function() {
+        qwest.post('/api/categories/', this.state, {dataType:'json'})
+            .then((xhr, response) => { alert('Kategoria dodana!'); })
+    },
     render: function() {
-        return (<div>
-            <TopBar />
-            <ReviewList />
+        var attributes = this.state.attributes.map(
+            attr => <li>{attr}</li>
+        );
+
+        return (
+            <div>
+                <h1>Dodaj kategorię {this.state.name}</h1>
+                <input ref="nazwa" placeholder="Nazwa kategorii" onChange={this.updateTitle} />
+                <ul>{attributes}</ul>
+                <input ref="atrybut" onKeyDown={this.addAttribute} placeholder="Dodaj atrybut" /><br />
+                <button onClick={this.addCategory}>Dodaj</button>
             </div>
         );
     }
 });
 
-var TopBar = React.createClass({
+var ProductForm = React.createClass({
+    getInitialState: function() {
+        return {category_id:0, attributes:[], categories:[]};
+    },
+    componentDidMount: function() {
+        qwest.get('/api/categories/').then((xhr, response) => { 
+            this.setState(response); 
+            this.showFields();
+        });
+    },
+    showFields: function() {
+        var url = `/api/categories/${this.refs.kategoria.value}/attributes`; 
+        qwest.get(url).then((xhr, response) => {
+            response.category_id = this.refs.kategoria.value;
+            this.setState(response);
+        });
+    },
+    addProduct: function() {
+        var attributes = {};
+        for (let attr of this.state.attributes)
+            attributes[attr.id] = this.refs[attr.id].value;
+
+        var newProduct = {
+            name: this.refs.nazwa.value,
+            category_id: this.state.category_id,
+            attributes: attributes
+        };
+
+        qwest.post('/api/products/', newProduct, {dataType:'json'})
+            .then((xhr, response) => { alert('Produkt dodany!'); })
+    },
+    render: function() {
+        var categories = this.state.categories.map(
+            cat => <option value={cat.id}>{cat.name}</option>
+        );
+
+        var fields = this.state.attributes.map(
+            attr => <input ref={attr.id} placeholder={attr.name} />
+        );
+
+        return (
+            <div>
+                <h1>Dodaj produkt</h1>
+                <select ref="kategoria" onChange={this.showFields}>{categories}</select>
+                <input ref="nazwa" placeholder="Nazwa produktu" />
+                {fields}
+                <br /><button onClick={this.addProduct}>Dodaj produkt</button>
+            </div>
+        );
+    }
+});
+
+var Product = (props) => {
+    var attrs = [];
+
+    for (let attr in props)
+        if (attr != 'name' && attr != 'id')
+            attrs.push( <div className="attribute">{attr}: {props[attr]}</div> )
+
+    return (
+        <div className="item">
+            <h2>{props.name}</h2>
+            {attrs}
+        </div>
+    );
+
+};
+
+// -----------------------
+
+var ProductList = React.createClass({
+    getInitialState: function() {
+        return {categories:[], category_id:0, products: []};
+    },
+    componentDidMount: function() {
+        qwest.get('/api/categories/').then((xhr, response) => { 
+            this.setState(response); 
+            this.showProducts();
+        });
+    },
+    showProducts: function() {
+        var url = `/api/categories/${this.refs.kategoria.value}/products`; 
+        qwest.get(url).then((xhr, response) => {
+            response.category_id = this.refs.kategoria.value;
+            this.setState(response);
+        });
+    },
+    render: function() {
+        var categories = this.state.categories.map(
+            cat => <option value={cat.id}>{cat.name}</option>
+        );
+
+        var products = this.state.products
+            .map(prodData => <Product {...prodData} />);
+
+        return (
+            <div className="content-wrap">
+                <h1>Lista produktów</h1>
+                <select ref="kategoria" onChange={this.showProducts}>{categories}</select>
+                {products}
+            </div>
+        );
+    }
+});
+
+var AddProduct = React.createClass({
     render: function() {
         return (
-            <div id="top">
-                <span id="logo">Test</span>
+            <div className="content-wrap">
+                <ProductForm />
             </div>
         );
     }
 });
 
-var Review = React.createClass({
+var AddCategory = React.createClass({
     render: function() {
-        return <div className="review">nick: {this.props.nickname}<br />review: {this.props.text_review}</div>;
-    }
-});
-
-var ReviewList = React.createClass({
-    componentDidMount: function() {
-      qwest.get('/products/2/reviews')
-        .then((xhr, response) => { this.setState(response); })
-    },
-    getInitialState: function() {
-        return {reviews: []};
-    },
-    render: function() {
-        var list = this.state.reviews.map(
-            reviewData => { return <Review key={reviewData.nickname} {...reviewData}/>; }
+        return (
+            <div className="content-wrap">
+                <CategoryForm />
+            </div>
         );
-        return <div>{list}</div>;
     }
 });
 
+// -----------------------------------------------------------------
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+var App = (props) =>
+    <div>
+        <TopBar username="user 1" />
+        <MainMenu />
+        <main>{props.children}</main>
+    </div>;
+
+ReactDOM.render((
+    <Router history={createHistory()}>
+        <Route path="/" component={App}>
+            <Route path="produkty" component={ProductList} />
+            <Route path="produkty/dodaj" component={AddProduct} />
+            <Route path="kategorie/dodaj" component={AddCategory} />
+        </Route>
+    </Router>
+    ), document.body
 );
