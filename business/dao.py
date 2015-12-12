22df7817733c3b2e812c5ac87437cd617287c060
dwisniewski1993 <domwis@st.amu.edu.pl>
2015-12-12 20:54:16 +0100

    Refactor warstwy biznesowej
    Wydzielenie "repozytoriów", obsługa błędnych zapytań

---
+++ business/dao.py
@@ -0,0 +1,114 @@
+from flask import jsonify
+
+class Repository:
+    def __init__(self, connection):
+        self.connection = connection
+
+# --------------
+
+class CategoryRepository(Repository):
+    def get_all(self):
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM categories"
+            cursor.execute(sql)
+
+            return jsonify({'categories': cursor.fetchall()})
+
+    def get_attributes(self, category_id):
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM categories_attributes WHERE category_id = %s"
+            cursor.execute(sql, (category_id,))
+
+            return jsonify({'attributes': cursor.fetchall()})
+
+    def get_products(self, category_id):
+        with self.connection.cursor() as cursor:
+            cursor.callproc('category', args=(category_id, ''))
+
+            return jsonify({'products': cursor.fetchall()})
+
+    def add(self, new_category):
+        if not new_category['name']:
+            return jsonify({'error': 'Pole nazwa nie może być puste'}), 400
+
+        if not new_category['attributes']:
+            return jsonify({'error': 'Kategoria musi mieć co najmniej 1 atrybut'}), 400
+
+        with self.connection.cursor() as cursor:
+            sql = "INSERT INTO categories(name) VALUES (%s)"
+            cursor.execute(sql, (new_category['name'],))
+
+            new_category_id = cursor.lastrowid
+            sql = "INSERT INTO categories_attributes(category_id, name, value_type) VALUES (%s,%s,'string')"
+            attributes = [(new_category_id, attr) for attr in new_category['attributes']]
+            cursor.executemany(sql, attributes)
+        self.connection.commit()        
+
+        return '', 201 # Created
+
+# --------------
+
+class ProductRepository(Repository):
+    def get(self, product_id):
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM products WHERE id = %s"
+            cursor.execute(sql, (product_id,))
+            product = cursor.fetchone()
+
+            if product is None:
+                return jsonify({'error':'Nie odnaleziono produktu'}), 404
+
+            sql = '''SELECT name, value FROM product_attributes pa
+                LEFT JOIN categories_attributes ca
+                ON ca.id = pa.attribute_id
+                WHERE product_id = %s'''
+
+            cursor.execute(sql, (product_id,))            
+            attributes = cursor.fetchall()
+
+            product['attributes'] = attributes
+            
+            return jsonify(product)
+
+    def add(self, prod):
+        if not prod['name']:
+            return jsonify({'error':'Nazwa produktu nie może być pusta'}), 400
+
+        print(prod['attributes'])
+
+        for key, value in prod['attributes'].items():
+            if not value:
+                return jsonify({'error':'Żaden z atrybutów nie może być pusty'}), 400
+
+        with self.connection.cursor() as cursor:
+            sql = "INSERT INTO products(name, category_id) VALUES (%s,%s)"
+            cursor.execute(sql, (prod['name'], prod['category_id']))
+            product_id = cursor.lastrowid
+
+            sql = "INSERT INTO product_attributes VALUES (%s,%s,%s)"
+            attributes_key_val = [(product_id,k,v) for k,v in prod['attributes'].items()]
+            cursor.executemany(sql, attributes_key_val)
+        self.connection.commit()        
+
+        return '', 201
+
+# --------------
+
+class ReviewRepository(Repository):
+    def get(self, product_id):
+        with self.connection.cursor() as cursor:
+            sql = "SELECT * FROM reviews WHERE product_id = %s"
+            cursor.execute(sql, (product_id,))
+
+            return jsonify({'reviews': cursor.fetchall()})
+
+    def add(self, rev):
+        if not rev['nick'] or not rev['comment']:
+            return jsonify({'error':'Żadne z pól nie powinno być puste'}), 400 # Bad request
+
+        with self.connection.cursor() as cursor:
+            sql = "INSERT INTO reviews(product_id, nick, rating, comment) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (rev['pid'], rev['nick'], rev['rating'], rev['comment']))
+        self.connection.commit()
+
+        return '', 201 # Created
--- business/server.py
+++ business/server.py
@@ -1,105 +1,52 @@
-from flask import Flask, request, jsonify, send_from_directory
-from model import *
-from services import Repository
+from flask import Flask, request, jsonify
+import dao
 import pymysql
 
 connection = pymysql.connect(host='46.101.220.96', user='root', password='projekt tas', db='tas', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
 app = Flask(__name__, static_url_path='/static', static_folder='../web/')
 
+reviews = dao.ReviewRepository(connection)
+categories = dao.CategoryRepository(connection)
+products = dao.ProductRepository(connection)
+
+# ------------
 
 @app.route('/api/categories/', methods=['GET', 'POST'])
 def get_categories():
     if request.method == 'GET':
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM categories"
-            cursor.execute(sql)
-
-            return jsonify({'categories': cursor.fetchall()})
-    else:
-        new_category = request.json
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO categories(name) VALUES (%s)"
-            cursor.execute(sql, (new_category['name'],))
-            sql = "INSERT INTO categories_attributes(category_id, name, value_type) VALUES (%s,%s,'string')"
-            cursor.executemany(sql, [(cursor.lastrowid,a) for a in new_category['attributes']])
-        connection.commit()        
-
-        return jsonify({'status':'ok'})
-
-@app.route('/api/categories/<int:category_id>/')
-def get_category(category_id):
-    with connection.cursor() as cursor:
-        sql = "SELECT name FROM categories WHERE id = %s"
-        cursor.execute(sql, (category_id,))
-
-        return jsonify(cursor.fetchone())
-
+        return categories.get_all()
+    elif request.method == 'POST':
+        return categories.add(request.json)
 
 @app.route('/api/categories/<int:category_id>/attributes')
 def categories_attributes(category_id):
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM categories_attributes WHERE category_id = %s"
-        cursor.execute(sql, (category_id,))
-
-        return jsonify({'attributes': cursor.fetchall()})
-
-
-@app.route('/api/products/', methods=['POST'])
-def add_product():
-    new = request.json
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO products(name, category_id) VALUES (%s,%s)"
-        cursor.execute(sql, (new['name'],new['category_id']))
-        sql = "INSERT INTO product_attributes VALUES (%s,%s,%s)"
-        cursor.executemany(sql, [(cursor.lastrowid,k,v) for k,v in new['attributes'].items()])
-    connection.commit()        
-
-    return jsonify({'status':'ok'})
-
-@app.route('/api/products/<int:product_id>/')
-def get_product(product_id):
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM products WHERE id = %s"
-        cursor.execute(sql, (product_id,))
-        product = cursor.fetchone()
-
-        sql = '''SELECT name, value FROM product_attributes pa
-            LEFT JOIN categories_attributes ca
-            ON ca.id = pa.attribute_id
-            WHERE product_id = %s'''
-
-        cursor.execute(sql, (product_id,))            
-        attributes = cursor.fetchall()
-
-        product['attributes'] = attributes
-        
-        return jsonify(product)
-
-
-@app.route('/api/products/<int:product_id>/reviews')
-def get_product_reviews(product_id):
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM reviews WHERE product_id = %s"
-        cursor.execute(sql, (product_id,))
-
-        return jsonify({'reviews': cursor.fetchall()})
-
-@app.route('/api/reviews/', methods=['POST'])
-def add_review():
-    rev = request.json
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO reviews(product_id, nick, rating, comment) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (rev['pid'], rev['nick'], rev['rating'], rev['comment']))
-    connection.commit()        
-
-    return jsonify({'status':'ok'})
+    return categories.get_attributes(category_id)
 
 @app.route('/api/categories/<int:category_id>/products')
 def get_category_products(category_id):
-    with connection.cursor() as cursor:
-        cursor.callproc('category', args=(category_id, ''))
-        return jsonify({'products': cursor.fetchall()})
+    return categories.get_products(category_id)
 
+# ------------
+
+@app.route('/api/products/', methods=['POST'])
+def add_product():
+    return products.add(request.json)
+
+@app.route('/api/products/<int:product_id>/')
+def get_product(product_id):
+    return products.get(product_id)
+
+# ------------
+
+@app.route('/api/products/<int:product_id>/reviews')
+def get_product_reviews(product_id):
+    return reviews.get(product_id)
+
+@app.route('/api/reviews/', methods=['POST'])
+def add_review():
+    return reviews.add(request.json)
+
+# ------------
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
